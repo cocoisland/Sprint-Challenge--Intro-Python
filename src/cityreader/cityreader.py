ffb5ffc557b3d5a4fca70d6c47b46fd513d74e0b
Tony Tia <webmaster@tonytia.com>
2019-05-31 10:58:58 -0700

    update

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -58,7 +58,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c.name, c.lat, c.lon)
+    print(f'{c.name} {c.lat} {c.lon}')
 
 # STRETCH GOAL!
 #
